e30cbd91c14e4b27b8656546bcfb9b63e199498c
Marcio Ferreira <marcio@ebi.ac.uk>
2021-12-23 23:26:35 -0400

    finished csv

--- fish_lane_analyzer.py
+++ fish_lane_analyzer.py
@@ -1,5 +1,5 @@
-
 import scipy.stats
+import pathlib
 from skimage import io
 import cv2
 import numpy as np
@@ -7,15 +7,53 @@ import math
 import pandas as pd
 import time
 from matplotlib import pyplot as plt
+
+# the 3 lines bellow you need to configure as your needs
+# the path of the images stack. Must end with .tif
+path_for_images = 'C:/Users/marci/Documents/projetos_code/bands fish/videos/20210921_ip10-1_4_MMStack_Default.ome.tif'
+# how many frames to quit when analyzing.
+# Using every frame can increase too much the variability of the data. We suggest at least 3.
+frame_space = 3
+# can make adjustment if the position of the camera changes
 list_grid = [0, 138, 261, 390, 519, 636, 762, 897,
              1008, 1140, 1263, 1383, 1515, 1638, 1761, 1900]
+
+
+def normVideo(frames):
+
+    norm_frames = []
+
+    #max_in_frames = np.max(frames)
+
+    read_frames = []
+    for i in range(0, len(frames), frame_space):
+        read_frames.append(i)
+
+        frame = frames[i]
+
+        if (frame is not None) and (frame.size > 0):
+
+            image_enhanced = cv2.equalizeHist(frame)
+            image_enhanced = cv2.bilateralFilter(image_enhanced, 9, 100, 100)
+
+        norm_frames.append(image_enhanced)
+
+    return norm_frames, read_frames
 
 
 backSub = cv2.createBackgroundSubtractorMOG2(
     history=50, varThreshold=20, detectShadows=True)
 #backSub = cv2.createBackgroundSubtractorKNN(history = 10, dist2Threshold = 800.0, detectShadows = False)
-ret, images = cv2.imreadmulti(
-    'C:/Users/marci/Documents/projetos_code/bands fish/videos/histo.tif', [], cv2.IMREAD_GRAYSCALE)
+ret, images = cv2.imreadmulti(path_for_images, [], cv2.IMREAD_GRAYSCALE)
+
+final_path = pathlib.PurePath(path_for_images)
+file_name = final_path.name
+
+with open(file_name + '.csv', 'a') as fd:
+    fd.write('frame_number, fish_1, fish_2,fish_3,fish_4,fish_5,fish_6,fish_7,fish_8,fish_9,fish_10,fish_11,fish_12,fish_13,fish_14,fish_15\n')
+
+# normalize the frames
+images_norm, read_frames = normVideo(images)
 
 
 fish = {"fish_1": None, "fish_2": None, "fish_3": None, "fish_4": None, "fish_5": None, "fish_6": None, "fish_7": None,
@@ -23,11 +61,11 @@ fish = {"fish_1": None, "fish_2": None, "fish_3": None, "fish_4": None, "fish_5"
 
 video_final = []
 
-for idx, image in enumerate(images):
+for idxf, image in enumerate(images_norm):
 
     fgMask = backSub.apply(image)
 
-    if idx > 4:
+    if idxf > 4:
 
         provisional_fish = {"fish_1": None, "fish_2": None, "fish_3": None, "fish_4": None, "fish_5": None, "fish_6": None, "fish_7": None,
                             "fish_8": None, "fish_9": None, "fish_10": None, "fish_11": None, "fish_12": None, "fish_13": None, "fish_14": None, "fish_15": None}
@@ -36,7 +74,7 @@ for idx, image in enumerate(images):
 
         filtered_contours = []
 
-        image = cv2.GaussianBlur(image, (7, 7), 0)
+        image = cv2.GaussianBlur(image, (13, 13), 0)
 
         contours, hierarchy = cv2.findContours(
             fgMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -68,16 +106,23 @@ for idx, image in enumerate(images):
         right_values = []
         for key, value in provisional_fish.items():
             if value is not None:
-                
+
                 # decide if right or left based on fish dictionary (fish os previous)
                 if fish[key] is not None:
-                    if value[0][0] < (fish[key][0] - 2):
+                    if value[0][0] < (fish[key][0] - 5):
                         left.append(key)
-                        left_values.append(int(abs(value[0][0] - fish[key][0])))
-                    elif value[0][0] > (fish[key][0] + 2):
+                        result_left = int(abs(value[0][0] - fish[key][0]))
+                        if result_left > 40:
+                            result_left = 40
+                        left_values.append(result_left)
+                    elif value[0][0] > (fish[key][0] + 5
+                                        ):
                         right.append(key)
-                        right_values.append(int(abs(value[0][0] - fish[key][0])))
-                
+                        result_right = int(abs(value[0][0] - fish[key][0]))
+                        if result_right > 40:
+                            result_right = 40
+                        right_values.append(result_right)
+
                 if len(value) > 1 and fish[key] is not None:
                     results_distance = []
 
@@ -90,27 +135,40 @@ for idx, image in enumerate(images):
                     fish.update({key: value[min_index]})
                 else:
                     fish.update({key: value[0]})
-                
+
             else:
                 invalids.append(key)
-                
+
+        final_row = [read_frames[idxf]]
         for item in fish.items():
             if item[1] is not None:
                 if item[0] not in invalids:
                     if item[0] in left:
                         value_index = left.index(item[0])
-                        cv2.circle(img_3channels, (item[1][0], item[1][1]), left_values[value_index], (255, 0, 0), 5)                        
+                        cv2.circle(
+                            img_3channels, (item[1][0], item[1][1]), left_values[value_index], (255, 0, 0), 5)
+                        final_row.append(-1 * int(left_values[value_index]))
                     elif item[0] in right:
                         value_index = right.index(item[0])
-                        cv2.circle(img_3channels, (item[1][0], item[1][1]), right_values[value_index], (0, 255, 0), 5)     
-                        #cv2.circle(img_3channels, (item[1][0], item[1][1]), 20, (0, 255, 0), 5)
+                        cv2.circle(
+                            img_3channels, (item[1][0], item[1][1]), right_values[value_index], (0, 255, 0), 5)
+                        final_row.append(int(right_values[value_index]))
                     else:
-                        cv2.circle(img_3channels, (item[1][0], item[1][1]), 5, (0, 0, 255), 5)
+                        cv2.circle(img_3channels,
+                                   (item[1][0], item[1][1]), 5, (0, 0, 255), 5)
+                        final_row.append(0)
                 else:
-                    cv2.circle(img_3channels, (item[1][0], item[1][1]), 5, (0, 0, 255), 5)
-                    
-        
-            
+                    cv2.circle(img_3channels,
+                               (item[1][0], item[1][1]), 5, (0, 0, 255), 5)
+                    final_row.append(0)
+            else:
+                final_row.append(0)
+
+        with open(file_name + '.csv', 'a') as fd:
+            my_str = ','.join(str(x) for x in final_row)
+            my_str = my_str + '\n'
+            fd.write(my_str)
+
         img_w = img_3channels[0].shape[0]
         for coord in list_grid:
             start_point = (0, coord)
